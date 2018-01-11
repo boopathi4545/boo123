8d1758ab69cd72a41ccaabddc197a690b3323971
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-01-11 10:35:02 +0000

    Create pali.c

---
+++ pali.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,i,a,r=0;
+scanf("%d",&n);
+for(i=n;i>0)
+{
+a=n%10;
+r=r*10=a;
+n/10;
+}
+if(n=r)
+{
+printf("yes");
+else
+{
+printf("no");
+}
+return 0;
+}
