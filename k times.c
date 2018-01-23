d0c048cf4003ee2fe74fb197cf49f6599264e15c
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-01-23 10:30:37 +0000

    Create k times.c

---
+++ k times.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+char m[50];
+printf("entr the string:");
+scanf("%s",&m);
+printf("enter the value:");
+scanf("%d",&j);
+for(i=0;i<=j;i++)
+{
+printf("%s",m);
+}
+return 0;
+}
