99191b23efb47a1d16c0f6224dc61ff5d31bfef1
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-01-25 10:40:14 +0000

    Create compare two.c

---
+++ compare two.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50],b[50];
+int x,y;
+x=strlen(a);
+y=strlen(b);
+if(x>y)
+{
+printf("%s",x);
+}
+else
+{
+printf("%s",y);
+}
+returan 0;
+}
