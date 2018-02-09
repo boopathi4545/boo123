97e59a6011b7a3e9d0692bfdb61eac77eee866c8
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-02-09 14:22:18 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char s[50];
+int n,i;
+printf("enter the string\n");
+scanf("%s",&s[i]);
+printf("enter the limit\n");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("%s",s[i]);
+}
+return 0;
+}
