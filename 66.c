3280e0aacbffbcc3d7e3c6bfa84c7f24e3c06d4b
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-02-13 15:03:55 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,count=0;
+	scanf("%d",&a);
+	for(b=1;b<=a;b++)
+	{
+		if(a%b==0)
+		{
+			count++;
+		}
+	}
+	if(count==2)
+	{
+		printf("prime");
+	}
+	else
+	{
+		printf("not prime");
+	}return 0;
+}
