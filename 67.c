bae519586b4272883e5bb702501b093ce8865abe
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-02-22 10:24:43 +0000

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the value\n");
+    scanf("%d",&n);
+    while(m!=0)
+    {
+        if(n%10==0)
+        {
+            printf("%d",n);
+            break;
+        }
+        n++;
+    }
+  
+}
