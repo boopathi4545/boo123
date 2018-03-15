0561d79f3e6efbf4b95724525eed127b097868e7
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-03-15 16:31:41 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+	int l,b,h;
+	scanf("%d %d %d",&l,&b,&h);
+	int v,tsa;
+	v=l*b*h;
+	tsa=2*((l*b)+(b*h)+(h*l));
+	printf("\nVOLUME=%d",v);
+	printf("\nTSA=%d",tsa);
+	return 0;
+}
